9cdb991e9d459f6291e61cdb9057f57aceb63636
Thompson Edolo <verygreenboi@live.com>
2014-02-24 14:48:48 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CallCenterApp::Application.config.secret_key_base = 'b2415241e05cb982fb14297809aeab844646017493bcb9e470672842b7fd9e4d8c0daf61ea9906898fa0ac757d0fdd28edc9337487c1d48b084ebcb77908453a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CallCenterApp::Application.config.session_store :cookie_store, key: '_call_center_app_session'
