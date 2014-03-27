3d3e3d08859781f21982bc026eeba85ccadf9f02
Thompson Edolo <verygreenboi@live.com>
2014-03-27 11:26:07 +0100

    Tightened up the admin panel

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,9 +7,9 @@ class DashboardController < ApplicationController
 	  def load_perm_scope(user)
 	  	if user.has_role? :admin
 	  		@role = "Admin"
-	  		@a = Account.all.limit(3)
-	  		@b = Bank.all.limit(3)
-	  		@u = User.all.where.not(id: user.id).limit(3)
+	  		@a = Account.all.order(created_at: :desc).limit(3)
+	  		@b = Bank.all.order(created_at: :desc).limit(3)
+	  		@u = User.all.where.not(id: user.id).order(created_at: :desc).limit(3)
 	  	else
 	  		@role = "User"
 	  	end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	before_filter :authenticate_user!
-  load_and_authorize_resource
+  load_and_authorize_resource :only => [:index, :show, :new]
 
   def index
     @users = User.all
@@ -17,13 +17,27 @@ class UsersController < ApplicationController
 		
 	end
 	def edit
-		
+		@user = User.find(params[:id])
 	end
 	def update
-		
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			# Handle Update
+			flash[:notice] = "User updated"
+			redirect_to @user
+		else
+			render 'edit'
+		end
 	end
 	def destroy
-		
+		User.find(params[:id]).destroy
+    flash[:notice] = "User deleted."
+    redirect_to root_url
 	end
+
+	private
+		def user_params
+			params.require(:user).permit!
+		end
 
 end
